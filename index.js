70999d7baf4bb3b53e24b6d68656affaf18aa212
Jenny Thai <jennythai.fw@gmail.com>
2023-12-06 21:08:41 +0700

    "done"

--- index.js
+++ index.js
@@ -194,6 +194,6 @@ function broadcastPlayerList() {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
